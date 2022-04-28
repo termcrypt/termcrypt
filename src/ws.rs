b69d347f09afe54af49811562665a2f125289fb9
NomNomBiscuit <72892531+peepopoggers@users.noreply.github.com>
2022-04-28 22:49:41 +0100

    Testing

--- src/ws.rs
+++ src/ws.rs
@@ -1,7 +1,7 @@
-use super::bybit_exchange::bybit_ws::*;
+use crate::bybit_core::bybit_ws::*;
 use anyhow::{Error as AnyHowError, Result};
 
-use crate::utils::termbug;
+//use crate::utils::termbug;
 
 pub async fn websocket_init(
 	bybit_pub_key: String,
@@ -11,7 +11,7 @@ pub async fn websocket_init(
 		match bybit_websocket(bybit_pub_key, bybit_priv_key).await {
 			Ok(_x) => { /*:L*/ }
 			Err(_e) => {
-				println!();
+				todo!("Websocket init failed: {}", _e);
 				//termbug::error("FATAL Bybit websocket error (Opening trades is unsafe) {_e:?}");
 			}
 		}
