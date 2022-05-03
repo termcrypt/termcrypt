5a8fa89e52046753505b4d05e961edb36523f451
NomNomBiscuit <72892531+peepopoggers@users.noreply.github.com>
2022-05-03 18:57:56 +0100

    Made termcrypt not require bybit API keys

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
 #![windows_subsystem = "console"]
 #![crate_type = "bin"]
-use bybit::http::{self, Client as BybitClient};
+use bybit::http::Client as BybitClient;
 use anyhow::{Error as AnyHowError, Result as AnyHowResult};
-use serde::{Deserialize, Serialize};
+//use serde::{Deserialize, Serialize};
 use std::{io::{self}, env, fs, time::{Duration}};
 use tui::{
     backend::CrosstermBackend,
     Terminal,
 };
 use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture},
+    event::{DisableMouseCapture, EnableMouseCapture},
 	cursor::DisableBlinking,
     execute,
     terminal::{self, disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 
+// Exchanges
 mod bybit_core;
+mod ftx_core;
+
+// Utils
 mod db;
 mod misc_commands;
 mod sync;
@@ -25,7 +29,10 @@ mod userspace;
 mod command_handling;
 mod orders;
 
+// Exchanges
 use bybit_core::{bybit_commands::{self, BybitStruct}};
+use ftx_core::{ftx_commands::{self, FtxStruct}};
+
 use db::{get_db_info, history_location};
 use sync::*;
 use utils::{termbug, terminal_width};
@@ -37,26 +44,43 @@ static VERSION: &str = env!("CARGO_PKG_VERSION");
 static REPO: &str = "https://github.com/termcrypt/termcrypt";
 
 // Values returned from initial database query
-#[derive(Clone, Debug)]
+#[derive(Debug, Clone)]
 pub struct Config {
-	// Default user pair for Bybit
+	// Exchange specific
+
+	// Bybit
+	pub bybit_api: Option<BybitClient>,
 	pub bybit_default_pair: String,
-	// Default user sub-account for Bybit
 	pub bybit_default_sub: String,
-	// Public API key for Bybit
-	pub bybit_pub_key: String,
-	// Private API key for Bybit
-	pub bybit_priv_key: String,
-	// User config ratio warn number;
+
+	// Ftx
+	pub ftx_default_pair: String,
+	pub ftx_default_sub: String,
+	/*
+	pub ftx_pub_key: String,
+	pub ftx_priv_key: String,
+	pub ftx_default_pair: String,
+	pub ftx_default_sub: String,
+	*/
 	// Warns user if trade ratio is too low
 	pub ratio_warn_num: f64,
 }
 
 // Available exchanges; bool inside is for if that exchange is enabled (set up) or not
-#[derive(Serialize, Deserialize, Debug)]
-pub enum ActiveExchanges {
-	Bybit(bool),
-	Ftx(bool),
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum Exchange {
+	Bybit,
+	Ftx,
+}
+
+impl Exchange {
+	const VALUES: [Self; 2] = [Self::Bybit, Self::Ftx];
+	fn to_string(&self) -> String {
+		match self {
+			Exchange::Bybit => {"Bybit".to_string()},
+			Exchange::Ftx => {"Ftx".to_string()},
+		}
+	}
 }
 
 // UI display arrangements
@@ -69,24 +93,19 @@ enum UIModes {
 	EventsOnly
 }
 
-// User interface location for function focus
-pub enum UILocation {
-	// Main output stream
-	Main,
-	// Background output stream (fills, etc)
-	Events
-}
-
 // Userspace state
+#[derive(Debug, Clone)]
 pub struct UserSpace {
 	// Database information
 	pub db_info: Config,
+	// Currently active exchange
+	pub active_exchange: Exchange,
 	// Current pair name
 	pub pair: String,
 	// Current sub-account name
 	pub sub_account: String,
 	// Bybit API 
-	pub bybit_api: BybitClient,
+	//pub bybit_api: BybitClient,
 	// Boolean for if the terminal is wide enough for ascii
 	pub desktop_terminal: bool,
 	// Current user input before submission
@@ -132,35 +151,25 @@ async fn main() -> AnyHowResult<(), AnyHowError> {
 	}
 
 	// Initiates database
-	let db_info = get_db_info(true).await?;
-	let mut command_history = Vec::new();
+	let db_info = get_db_info().await?;
 
-	// Start bybit api instance
-	let mut bybit_api = http::Client::new(
-		http::MAINNET_BYBIT,
-		&db_info.bybit_pub_key,
-		&db_info.bybit_priv_key,
-	).unwrap();
-
-	// Synchronize missed executions when app was not running
-	match startup_sync(Some(&mut bybit_api)).await {
-		Ok(_x) => { /*:L*/ }
-		Err(_e) => {
-			command_history.insert(0, format!("Failed to sync trades with ws, but termcrypt kept running: {_e:?}"));
-		}
-	};
+	/*
+	pair: db_info.bybit_default_pair.to_owned(),
+		sub_account: db_info.bybit_default_sub.to_owned(),
+		input_prefix: format!("[{}]({})>", db_info.bybit_default_sub, db_info.bybit_default_pair),
+	*/
 
 	// Initiates userspace (UI and main loop)
 	let mut userspace = UserSpace {
-		pair: db_info.bybit_default_pair.to_owned(),
-		sub_account: db_info.bybit_default_sub.to_owned(),
-		input_prefix: format!("[{}]({})>", db_info.bybit_default_sub, db_info.bybit_default_pair),
+		active_exchange: Exchange::Bybit,
+		pair: "".to_string(),
+		sub_account: "main".to_string(),
+		input_prefix: "".to_string(),
 		db_info,
 		// Initiates Bybit API (note: make option on multi exchanges)
-		bybit_api,
 		desktop_terminal,
         input: String::new(),
-		command_history,
+		command_history: Vec::new(),
 		command_history_scroll_overflow: Vec::new(),
 		command_count: 0,
 		event_history: vec![
@@ -170,7 +179,7 @@ async fn main() -> AnyHowResult<(), AnyHowError> {
 			("at 2069$ (ETH)".to_string(), EventLogType::TpFill),
 			
 		],
-		tick_rate: Duration::from_millis(50),
+		tick_rate: Duration::from_millis(6),
 		stream_differ: 0,
 		input_old: String::new(),
 	};
