71889b470139c8fe02193e218f0ca9d6afb05ef8
NomNomBiscuit <72892531+peepopoggers@users.noreply.github.com>
2022-05-03 18:56:35 +0100

    Setting up ftx core to support modular system

--- src/ftx_core/ftx_commands.rs
+++ src/ftx_core/ftx_commands.rs
@@ -1,1 +1,21 @@
-//...
+use anyhow::{bail, Error as AnyHowError, Result as AnyHowResult};
+use async_trait::async_trait;
+use tui::{
+    backend::Backend,
+    Terminal,
+};
+use chrono::Utc;
+use crate::{
+    UserSpace,
+    utils::{yn},
+    db::*,
+    orders::*,
+    command_handling::CommandHandling
+};
+
+pub struct FtxStruct {}
+
+#[async_trait]
+impl<B: Backend + std::marker::Send> CommandHandling<B> for FtxStruct {
+    //...
+}
